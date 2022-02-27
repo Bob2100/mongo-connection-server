534e72d694ba1c97ddd176baf86d290206705def
Bob <bob_w@qq.com>
2022-02-27 08:41:31 +0800

    fix bug

--- src/dao/findA.js
+++ src/dao/findA.js
@@ -1,4 +1,4 @@
-import utils from '../utils'
+import utils from '../utils/index.js'
 const client = utils.db.getClient()
 
 async function run() {
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 const uri = 'mongodb://localhost:27017'
-const client = null
+let client = null
 
 export default {
   getClient() {
@@ -8,5 +8,6 @@ export default {
       return client
     }
     client = new MongoClient(uri)
+    return client
   },
 }
