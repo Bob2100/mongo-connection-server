8a1ea5d71cd42b08898d785aff414b3a61bf0a65
Bob <bob_w@qq.com>
2022-02-27 20:28:18 +0800

    findOne

--- src/dao/BaseDao.js
+++ src/dao/BaseDao.js
@@ -1,3 +1,10 @@
+import dbUtils from '../utils/dbUtils.js'
 export default class BaseDao {
-  findOne() {}
+  findOne(db, collection, query, options) {
+    return dbUtils
+      .getClient()
+      .db(db)
+      .collection(collection)
+      .findOne(query, options)
+  }
 }
