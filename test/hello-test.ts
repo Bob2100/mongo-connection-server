7df45be44c0a45b19da27abe1b0456ed66dd5126
Bob <bob_w@qq.com>
2022-02-27 12:52:35 +0800

    添加mocha测试

---
+++ test/hello-test.ts
@@ -0,0 +1,23 @@
+import utils from '../src/utils'
+const assert = require('assert')
+const { sum } = utils
+
+describe('#hello.js', () => {
+  describe('#sum()', () => {
+    it('sum() should return 0', () => {
+      assert.strictEqual(sum(), 0)
+    })
+
+    it('sum(1) should return 1', () => {
+      assert.strictEqual(sum(1), 1)
+    })
+
+    it('sum(1, 2) should return 3', () => {
+      assert.strictEqual(sum(1, 2), 3)
+    })
+
+    it('sum(1, 2, 3) should return 6', () => {
+      assert.strictEqual(sum(1, 2, 3), 6)
+    })
+  })
+})
--- test/index.ts
+++
@@ -1,8 +0,0 @@
-import utils from '../src/utils'
-import assert = require('assert')
-const { sum } = utils
-
-assert.strictEqual(sum(), 0)
-assert.strictEqual(sum(1), 1)
-assert.strictEqual(sum(1, 2), 3)
-assert.strictEqual(sum(1, 2, 3), 6)
