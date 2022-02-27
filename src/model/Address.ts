b31bc41e489849e8a07191480855af992d020278
Bob <bob_w@qq.com>
2022-02-27 14:26:05 +0800

    添加model

---
+++ src/model/Address.ts
@@ -0,0 +1,5 @@
+export default class Address {
+  _id: string
+  name: string
+  location: string
+}
---
+++ src/model/User.ts
@@ -0,0 +1,8 @@
+import Address from './Address'
+
+export default class User {
+  _id: string
+  name: string
+  age: string
+  addr: Address
+}
