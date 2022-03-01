d9e089f6e2bd653fece71e7012c01e4f3649f59c
吴文波 <wuwb@yuntongxun.com>
2022-03-01 20:41:51 +0800

    pass

--- test/baseDao-test.js
+++ test/baseDao-test.js
@@ -13,8 +13,9 @@ describe('baseDao', () => {
       })
     })
     it('findOne() should return a doc', async () => {
+      console.log(typeof insertRes.insertedId)
       const doc = await baseDao.findOne('test', 'fruits', {
-        _id: insertRes.insertId,
+        _id: insertRes.insertedId,
       })
       assert.notStrictEqual(doc, null)
     })
