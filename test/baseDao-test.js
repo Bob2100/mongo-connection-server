0567f6c7cf81f294a1a4345c74e45acd00a5af14
吴文波 <wuwb@yuntongxun.com>
2022-03-01 20:40:05 +0800

    update

--- test/baseDao-test.js
+++ test/baseDao-test.js
@@ -6,25 +6,17 @@ describe('baseDao', () => {
     baseDao.close()
   })
   describe('findOne()', () => {
-    it('findOne() should return a doc', async () => {
-      const doc = await baseDao.findOne(
-        'test',
-        'fruits',
-        { category: '水果' },
-        {
-          sort: { price: -1 },
-          projection: { _id: 0, name: 1, price: 1, category: 1 },
-        }
-      )
-      assert.notStrictEqual(doc, null)
-    })
-  })
-  describe('insertOne()', () => {
-    it('insertOne() should return a doc', async () => {
-      const res = await baseDao.insertOne('test', 'fruits', {
+    let insertRes = null
+    before(async () => {
+      insertRes = await baseDao.insertOne('test', 'fruits', {
         category: '水果',
       })
-      assert.notStrictEqual(res, null)
+    })
+    it('findOne() should return a doc', async () => {
+      const doc = await baseDao.findOne('test', 'fruits', {
+        _id: insertRes.insertId,
+      })
+      assert.notStrictEqual(doc, null)
     })
   })
 })
