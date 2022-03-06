7cd4aa0b1a63c0d29facccee5581c5ed9870d36a
Bob <bob_w@qq.com>
2022-03-06 23:01:13 +0800

    ts findOne

---
+++ src/dao/baseDaoTs.ts
@@ -0,0 +1,53 @@
+import ora from 'ora'
+import { FindOptions, MongoClient, ObjectId } from 'mongodb'
+import dbConfig from '../../config/db.js'
+import defaultData from './defaultData.js'
+
+let client: MongoClient = null
+
+async function getClient(uri = dbConfig.uri): Promise<MongoClient> {
+  if (client) {
+    return client
+  }
+  const spinner = ora(`连接 ${uri} ...\n`)
+  try {
+    client = new MongoClient(uri)
+    spinner.start()
+    await client.connect()
+    spinner.stop()
+    defaultData.init(client)
+    return client
+  } catch (error) {
+    client.close()
+    client = null
+    console.error(error)
+    spinner.stop()
+  }
+  return null
+}
+
+export interface Fruits {
+  _id: ObjectId
+  name: string
+  price: number
+  category: '水果' | '蔬菜'
+}
+
+async function findOne<T>(
+  dbName: string,
+  colName: string,
+  filter: T,
+  options?: FindOptions
+): Promise<T> {
+  const client = await getClient()
+  return await client
+    .db(dbName)
+    .collection<T>(colName)
+    .findOne<T>(filter, options)
+}
+
+const baseDaoTs = {
+  getClient,
+  findOne,
+}
+export default baseDaoTs
